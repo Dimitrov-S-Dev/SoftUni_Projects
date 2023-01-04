17a065bfb4291b25827a35bbc9c77ab8b91e879c
Stan <dimitrov.s.dev@gmail.com>
2023-01-04 23:20:58 +0200

    Selling_Tickets

--- Selling_Tickets/Selling_tickets.py
+++ Selling_Tickets/Selling_tickets.py
@@ -1,5 +1,11 @@
+def get_price(tickets):
+    return (tickets * TICKET_PRICE) + SERVICE_CHARGE
+
+
+SERVICE_CHARGE = 5
 TICKET_PRICE = 10
 tickets_remaining = 100
+
 while tickets_remaining:
     print(f"There are {tickets_remaining} remaining tickets.")
     name = input("What is your name?\n")
@@ -11,14 +17,12 @@ while tickets_remaining:
     except ValueError as err:
         print(f"Oh no we run into issue. Please try again.")
     else:
-        total_cost = TICKET_PRICE * ticket_req
-        print(f"Total cost of requested tickets is: ${total_cost}")
+        print(f"Total cost of requested tickets is: ${get_price(ticket_req)}")
         answer = input("Would you like to purchase them? Y/N \n")
         if answer.lower() == "y":
             print(f"SOLD!")
             tickets_remaining -= ticket_req
         else:
             print("Thank you for visiting {name}.")
-
 
 print(f"Sorry the tickets are all SOLD OUT!!!")
